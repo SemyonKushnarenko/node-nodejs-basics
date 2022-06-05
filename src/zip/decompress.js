7f13bb67e332a071517821f12f1b15dec69cfa0a
Trisemya <trisemya@gmail.com>
2022-06-05 21:37:09 +0300

    feat: add zip/decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,17 @@
+import { createGunzip } from 'zlib'
+import path from 'path'
+import { pipeline } from 'stream'
+import { createReadStream, createWriteStream } from 'fs'
+
 export const decompress = async () => {
-    // Write your code here 
-};
+    const dearchiveToFile = path.join(path.dirname(''), 'src', 'zip', 'files', 'fileToCompress.txt')
+    const archive = path.join(path.dirname(''), 'src', 'zip', 'files', 'archive.gz')
+
+    const source = createReadStream(archive)
+    const gunzip = createGunzip()
+    const destination = createWriteStream(dearchiveToFile)
+
+    pipeline(source, gunzip, destination, err => {
+        if (err) throw new Error(err.message)
+    })
+}
