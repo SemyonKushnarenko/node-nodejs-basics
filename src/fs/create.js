9819a6888ae3db8809e3c026c562870c5d303441
Trisemya <trisemya@gmail.com>
2022-06-05 17:27:34 +0300

    feat: add fs/create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,16 @@
+import path from 'path'
+import fsPromises from 'fs/promises'
+
+const pathToFreshFile = path.join(__dirname, 'src', 'fs', 'files', 'fresh.txt')
+
 export const create = async () => {
-    // Write your code here 
-};
+    await fsPromises.access(pathToFreshFile)
+    .then(() => {
+        throw new Error('FS operation failed')
+    })
+    .catch(async () => {
+        await fsPromises.writeFile(pathToFreshFile, 'I am fresh and young')
+    })
+}
+
+create()
